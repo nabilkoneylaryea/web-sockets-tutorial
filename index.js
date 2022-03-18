664838af2b71d2c8b411d2d2c59a8677cc2776f5
Nabil <nkoney14@outlook.com>
2022-03-18 13:44:35 -0400

    Added ngrok just use ngrok http 3001

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const socket = require("socket.io");
+const ngrok = require('ngrok');
 
 const app = express();
 
-PORT = 3000;
+const PORT = 3001;
+
 const server = app.listen(PORT, () => {
     console.log(`Starting server on port ${PORT} ...`);
-})
+    // ngrok.connect({addr: PORT, authtoken: '26Xb9mUt8AHaTq2EvyaEWbTlYCE_63tC2UUkYmxVpckYaEoNw'})
+    // .then((HOST) => {
+    //     console.log(`Access web application at this url: ${HOST}`);
+    // })
+    // .catch((error) => {
+    //     console.log(`Error: ${error}}`);
+    // });
+    
+});
 
 // Static files
 app.use(express.static('public')) // express middleware that lets you serve files from a folder
 
 // socket.io works by installing it on the front and backend -> want to use one both sides to establish socket connection; here's the setup
 
-let io = socket(server); // takes server you want to work with as the paramter
+io = socket(server); // takes server you want to work with as the paramter
 
 io.on('connection', (socket) => { // listens for "connection" event and fires callback function; "socket" is socket instance; client's particular socket; won't work until setup on frontend
     console.log('Socket connection:', socket.id)
